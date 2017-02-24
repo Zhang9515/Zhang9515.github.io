43720cc6d5f68f6fc11345d3b9fd570b352631d5
SerhiiStets <researchergg@gmail.com>
2017-02-24 02:08:45 +0200

    add places for google map

---
+++ js/places.js
@@ -0,0 +1,43 @@
+var Kiev = {lat: 50.453314, lng: 30.542264};
+
+var Odessa = {lat: 46.484481, lng: 30.724144};
+
+var Chernivtsi = {lat: 48.292554, lng: 25.934947};
+
+var Lviv = {lat: 49.839532, lng: 24.029057};
+
+var Ternopil = {lat: 49.554475, lng: 25.597325};
+
+var Zhitomir = {lat: 50.254712, lng: 28.655864};
+
+var Zhitomir = {lat: 50.254712, lng: 28.655864};
+
+var Bila_Zerkva = {lat: 49.797099, lng: 30.135890};
+
+var Uman = {lat: 48.770581, lng: 30.215072};
+
+var Sevastopol = {lat: 44.614851, lng: 33.526761};
+
+var Simferopol = {lat: 44.951393, lng: 34.099599};
+
+var Herson = {lat: 46.635071, lng: 32.620210};
+
+var Lazurnoe = {lat: 46.076933, lng: 32.531247};
+
+var Vinniza = {lat: 49.232793, lng: 28.470320};
+
+var Poltava = {lat: 49.588003, lng: 34.544534};
+
+var Zaporizhya = {lat: 47.840760, lng: 35.138960};
+
+var Uzhgorod = {lat: 48.620511, lng: 22.287362};
+
+var Mukachevo = {lat: 48.449779, lng: 22.711857};
+
+var Ivano_F = {lat: 48.922166, lng: 24.713185};
+
+var Kam_Pod = {lat: 48.696659, lng: 26.580348};
+
+var Hmelnizkiy = {lat: 49.422815, lng: 26.991481};
+
+var Belgorod_Dn = {lat: 46.185652, lng: 30.346409};
--- js/script.js
+++ js/script.js
@@ -2,34 +2,33 @@
 var map;
 
 $(function() {
-    updateTime();
-
     initMap();
 });
 
-function updateTime() {
-
-    var date = new Date();
-    var hour = date.getHours();
-    var minutes = date.getMinutes();
-
-    var time = "";
-    time = (hour < 10) ? "0" + hour + ':' : hour + ':';
-    time += (minutes < 10) ? "0" + minutes : minutes;
-
-    $(".time").html(time);
-
-    setTimeout(updateTime, 1000);
-}
-
-
 
 function initMap() {
+
+
+    var list = [Kiev, Odessa, Chernivtsi, Lviv, Ternopil, Zhitomir, Bila_Zerkva, Uman, Sevastopol, Simferopol,
+        Herson, Lazurnoe, Vinniza, Poltava, Zaporizhya, Uzhgorod, Mukachevo, Ivano_F, Kam_Pod, Hmelnizkiy, Belgorod_Dn,
+
+    ]
+
     map = new google.maps.Map(document.getElementById('map'), {
         center: {
-            lat: -34.397,
-            lng: 150.644
+            lat: 50.453314,
+            lng: 30.542264
         },
-        zoom: 8
+        zoom: 3,
+        minZoom: 3,
+        maxZoom: 10
     });
+
+    list.forEach(function(place) {
+        new google.maps.Marker({
+            position: place,
+            map: map,
+        });
+    });
+
 }
