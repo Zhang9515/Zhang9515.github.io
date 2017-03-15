96444b17550a5d51c22f3fb514e5764771db2ba1
SerhiiStets <researchergg@gmail.com>
2017-03-15 09:57:09 +0200

    add copyright section

--- js/script.js
+++ js/script.js
@@ -3,7 +3,42 @@
 var map;
 
 $(function() {
+    menu_animation();
 
+
+
+    initMap();
+});
+
+
+function initMap() {
+
+    var list = [Kiev, Odessa, Chernivtsi, Lviv, Ternopil, Zhitomir, Bila_Zerkva, Uman, Sevastopol, Simferopol,
+        Herson, Lazurnoe, Vinniza, Poltava, Zaporizhya, Uzhgorod, Mukachevo, Ivano_F, Kam_Pod, Hmelnizkiy, Belgorod_Dn,
+        Yalta, Sudak, Alushta, Vien, Madrid, Barcelona, Zaragoza, Figueres, Collioure, Lisbon, Cascais, Sintra,
+        Cabo_da_Roca, Munchen, Timisoara, Hunedoara, Split, Omis, Trogir, Mostar, Budva, Brescia, Venice, Milan, Minsk,
+        Brest, Hatin, Antalya, Alanya, Athens, Varna
+    ]
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 48.198784,
+            lng: 13.213503
+        },
+        zoom: 4,
+        minZoom: 3,
+        maxZoom: 14
+    });
+
+    list.forEach(function(place) {
+        new google.maps.Marker({
+            position: place,
+            map: map,
+        });
+    });
+}
+
+function menu_animation() {
     $(".menu_main").click(function() {
         $('html, body').animate({
             scrollTop: 0
@@ -39,36 +74,5 @@ $(function() {
             scrollTop: $(".contact").offset().top
 
         }, 500);
-    });
-
-
-    initMap();
-});
-
-
-function initMap() {
-
-    var list = [Kiev, Odessa, Chernivtsi, Lviv, Ternopil, Zhitomir, Bila_Zerkva, Uman, Sevastopol, Simferopol,
-        Herson, Lazurnoe, Vinniza, Poltava, Zaporizhya, Uzhgorod, Mukachevo, Ivano_F, Kam_Pod, Hmelnizkiy, Belgorod_Dn,
-        Yalta, Sudak, Alushta, Vien, Madrid, Barcelona, Zaragoza, Figueres, Collioure, Lisbon, Cascais, Sintra,
-        Cabo_da_Roca, Munchen, Timisoara, Hunedoara, Split, Omis, Trogir, Mostar, Budva, Brescia, Venice, Milan, Minsk,
-        Brest, Hatin, Antalya, Alanya, Athens, Varna
-    ]
-
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {
-            lat: 48.198784,
-            lng: 13.213503
-        },
-        zoom: 4,
-        minZoom: 3,
-        maxZoom: 14
-    });
-
-    list.forEach(function(place) {
-        new google.maps.Marker({
-            position: place,
-            map: map,
-        });
     });
 }
